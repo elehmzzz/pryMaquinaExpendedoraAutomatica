98962f3e7813b2033e46b54809e2c052f16e3092
elehmzzz <elehmzzz@gmail.com>
2026-05-20 21:19:58 -0600

    Se creo la clase Producto con sus atributos correspondientes, sus propiedades, un constructor y un metodo llamado compra

--- Clases/Producto.cs
+++ Clases/Producto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,39 @@ namespace pryMaquinaExpendedora.Clases
 {
     internal class Producto
     {
+        private string nombreProducto;
+        private float costo;
+        private int existencia;
 
+        public string _nombreProducto{
+            get { return nombreProducto; }            
+        }
+        public float _costo {
+            get { return costo; }
+        }
+        public int _existencia {
+            get { return existencia; }
+        }
+
+
+        //constructor
+        public Producto(string nombreProducto, float costo, int existencia) {
+            this.nombreProducto = nombreProducto;
+            this.costo = costo;
+            this.existencia = existencia;
+        }
+
+        public string compra(int cantidad) {
+            if (cantidad > existencia)
+            {
+                return "No hay suficiente stock del producto.";
+            }
+            else {
+                existencia = existencia - cantidad;
+                float cobroTotal = costo * cantidad; 
+                return $"Compra realizada con exito \n Cobro total: $ {cobroTotal}"; 
+            }
+        }
 
     }
 }
