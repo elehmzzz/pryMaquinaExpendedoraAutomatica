a9c1e4a7a3370a27b6cbcff6120dedc05613f632
elehmzzz <elehmzzz@gmail.com>
2026-05-20 23:50:27 -0600

    explicacion de las partes del codigo con comentarios y correccion de error en los mensajes para el usuario.

--- Clases/Producto.cs
+++ Clases/Producto.cs
@@ -7,43 +7,62 @@ using System.Threading.Tasks;
 
 namespace pryMaquinaExpendedora.Clases
 {
+    //para este caso el nombre del producto es un string porque se va ingresar texto
+    //en el costo es un flotante, porque al ser un precio puede tener decimales
+    //y para la existencia es un entero, porque no puede haber una fraccion de un producto
+    //Aplicacion de encapsulamiento
+    //las variables son privadas porque no se quiere modificar directamente en el programa principal
+    //ya que no cualquiera puede modificar el costo o la existencia de un producto
+    //solo se aplico la propiedad de get para mostrar las variables pero no modificarlas
     internal class Producto
     {
+        //atributos
         private string nombreProducto;
         private float costo;
         private int existencia;
 
-        public string _nombreProducto{
-            get { return nombreProducto; }            
+        //propiedades para mostrar el nombre del producto, el costo y la existencia
+        public string _nombreProducto
+        {
+            get { return nombreProducto; }
         }
-        public float _costo {
+        public float _costo
+        {
             get { return costo; }
         }
-        public int _existencia {
+        public int _existencia
+        {
             get { return existencia; }
         }
 
 
-        //constructor
-        public Producto(string nombreProducto, float costo, int existencia) {
+        //constructor que recibe el nombre del producto, el costo y la existencia e inicializa las variables
+        public Producto(string nombreProducto, float costo, int existencia)
+        {
             this.nombreProducto = nombreProducto;
             this.costo = costo;
             this.existencia = existencia;
         }
 
-        public string compra(int cantidad) {
+        //metodo para comprar un producto, recibe la cantidad, returna la compra hecha y actualiza las existencia
+        public string compra(int cantidad)
+        {
             if (cantidad > existencia)
             {
-                return "No hay suficiente stock del producto.";
+                return "\n**** No hay suficiente stock del producto ****";
             }
-            else {
+            else
+            {
                 existencia = existencia - cantidad;
-                float cobroTotal = costo * cantidad; 
-                return $"Compra realizada con exito \n Cobro total: $ {cobroTotal}"; 
+                float cobroTotal = costo * cantidad;
+                return $"********* Compra realizada con exito ********* \nCobro total: $ {cobroTotal}";
             }
         }
-        public string mostrarProducto() {
-            return $"Producto: {nombreProducto} Precio: $ {costo} Existencia: {existencia}";
+
+        //metodo para mostrar el producto, el precio y la existencia
+        public string mostrarProducto()
+        {
+            return $"{nombreProducto} Precio: $ {costo} Existencia: {existencia}";
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -6,13 +6,14 @@ namespace pryMaquinaExpendedora
     {
         static void Main(string[] args)
         {
+            //creacion de 4 distintos productos
             Producto sabritasAdobadas = new Producto("Sabritas adobadas", 20, 15);
             Producto galletasChokis = new Producto("Chokis", 15, 20);
             Producto galletasArcoiris = new Producto("Arcoiris", 16, 25);
             Producto sabritasOrigial = new Producto("Sabritas original", 20, 25);
 
             string respuesta;
-
+            //ciclo de compra automatizada
             do
             {
                 Console.WriteLine("------------ Catalogo de productos ------------");
@@ -21,33 +22,37 @@ namespace pryMaquinaExpendedora
                 Console.WriteLine($"3. {galletasArcoiris.mostrarProducto()}");
                 Console.WriteLine($"4. {sabritasOrigial.mostrarProducto()}");
 
-
-                Console.Write("\nSelecciona el producto (1-4):");
+                Console.Write("\nSelecciona el producto (1-4): ");
                 int opc= int.Parse(Console.ReadLine());
-                Console.Write("ingrsa la cantidad a llevar: ");
+                Console.Write("ingresa la cantidad a llevar: ");
                 int cantidad = int.Parse(Console.ReadLine());
                 Console.WriteLine();
                 switch (opc)
                 {
                     case 1:
                         Console.WriteLine(sabritasAdobadas.compra(cantidad));
+                        Console.WriteLine(sabritasAdobadas.mostrarProducto());
                         break;
                     case 2:
                         Console.WriteLine(galletasChokis.compra(cantidad));
+                        Console.WriteLine(galletasChokis.mostrarProducto());
                         break;
                     case 3:
                         Console.WriteLine(galletasArcoiris.compra(cantidad));
+                        Console.WriteLine(galletasArcoiris.mostrarProducto());
                         break;
                     case 4:
                         Console.WriteLine(sabritasOrigial.compra(cantidad));
+                        Console.WriteLine(sabritasOrigial.mostrarProducto());
                         break;
                     default:
                         Console.WriteLine("Opcion no valida");
                         break;
                 }
 
-                Console.WriteLine("\n¿Quiere agregar otro producto?(si/no):");
+                Console.Write("\n¿Quiere agregar otro producto?(si/no): ");
                 respuesta = Console.ReadLine().ToUpper();
+                Console.WriteLine("\n");
             } while (respuesta == "SI");
 
         }
